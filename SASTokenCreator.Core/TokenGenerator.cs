511aba8962e0f56cfa0f82ca61a7f67e5a8c40c9
Nino Benvenuti <nino.benvenuti@gmail.com>
2016-04-04 23:21:19 -0400

    Initial commit of core and console.

---
+++ SASTokenCreator.Core/TokenGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace SASTokenCreator.Core
+{
+    public class TokenGenerator
+    {
+        public string CreateServiceBusSASToken(string resourceUri, string keyName, string key, int expiryPeriod = 3600)
+        {
+            TimeSpan sinceEpoch = DateTime.UtcNow - new DateTime(1970, 1, 1);
+            var expiry = Convert.ToString((int)sinceEpoch.TotalSeconds + expiryPeriod);
+
+            string stringToSign = HttpUtility.UrlEncode(resourceUri) + "\n" + expiry;
+            HMACSHA256 hmac = new HMACSHA256(Encoding.UTF8.GetBytes(key));
+
+            var signature = Convert.ToBase64String(hmac.ComputeHash(Encoding.UTF8.GetBytes(stringToSign)));
+            var sasToken = String.Format(CultureInfo.InvariantCulture,
+            "SharedAccessSignature sr={0}&sig={1}&se={2}&skn={3}",
+                HttpUtility.UrlEncode(resourceUri), HttpUtility.UrlEncode(signature), expiry, keyName);
+
+            return sasToken;
+        }
+    }
+}
---
+++ SASTokenGenerator.Console/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Globalization;
+using System.Text;
+using System.Web;
+using SASTokenCreator.Core;
+using CommandLineParser;
+using CommandLineParser.Arguments;
+
+namespace SASTokenCreator.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string _resourceUri = string.Empty;
+            string _keyName = string.Empty;
+            string _key = string.Empty;
+            int _expiryPeriod = 0;
+
+            CommandLineParser.CommandLineParser parser = new CommandLineParser.CommandLineParser();
+            parser.ShowUsageOnEmptyCommandline = true;
+
+            ValueArgument<string> resourceUri = new ValueArgument<string>('u', "resourceUri", "Set the URI to the SB resource");
+            resourceUri.Optional = false;
+
+            ValueArgument<string> keyName = new ValueArgument<string>('n',"keyName", "The name of the SAS key");
+            keyName.Optional = false;
+
+            ValueArgument<string> key = new ValueArgument<string>('k', "key", "The SAS key value");
+            key.Optional = false;
+
+            ValueArgument<int> expiryPeriod = new ValueArgument<int>('e', "expiryPeriod", "The expiry period, in seconds");
+            expiryPeriod.Optional = false;
+
+            parser.Arguments.Add(resourceUri);
+            parser.Arguments.Add(keyName);
+            parser.Arguments.Add(key);
+            parser.Arguments.Add(expiryPeriod);
+
+            parser.ParseCommandLine(args);
+
+            if (resourceUri.Parsed)
+            {
+                _resourceUri = resourceUri.Value;
+            }
+
+            if (keyName.Parsed)
+            {
+                _keyName = keyName.Value;
+            }
+
+            if (key.Parsed)
+            {
+                _key = key.Value;
+            }
+
+            if (expiryPeriod.Parsed)
+            {
+                _expiryPeriod = expiryPeriod.Value;
+            }
+
+            TokenGenerator tg = new TokenGenerator();
+            string sasToken = tg.CreateServiceBusSASToken(_resourceUri, _keyName, _key, _expiryPeriod);
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine(sasToken);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Enter to exit");
+            Console.ReadLine();
+        }
+
+    }
+}
